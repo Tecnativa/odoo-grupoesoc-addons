2ff53d2f44273dfdfe279ad63a9fadffee0bbe77
Jairo Llopis <j.llopis@grupoesoc.es>
2015-02-27 13:25:47 +0100

    Use right datetime module.

--- event_seats_per_registration/tests/test_limits.py
+++ event_seats_per_registration/tests/test_limits.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime
+from datetime import datetime
 from openerp.tests.common import TransactionCase
 from .. import exceptions as e
 

e4a73114420963c65ed68a34d5b3250fafa0d54c
Jairo Llopis <j.llopis@grupoesoc.es>
2015-06-18 10:13:37 +0200

    New README, raise version.

--- training/__openerp__.py
+++ training/__openerp__.py
@@ -18,7 +18,7 @@
 
 {
     "name": "Training",
-    "version": "2.1",
+    "version": "3.0",
     "category": "Project",
     "author": "Grupo ESOC",
     "license": "AGPL-3",
@@ -26,43 +26,6 @@
     "installable": True,
     "application": False,
     "summary": "Extend events with training capabilities",
-    "description": """
-Training
-========
-
-This module helps you organize your trainings.
-
-How does it work?
------------------
-
-This module installs the *event* module and extends it with training
-capabilities.
-
-A **training action** is a definition of a course. Thus, you can have a catalog
-of courses that you offer. Training actions define the **amount of hours** of
-each type that should be teached.
-
-Each training action can belong to a **training type**, which then defines what
-**type of hours** are expected in those training actions.
-
-For instance:
-
-- A training of type *on-site* may expect only *on-site* hours.
-
-- A training of type *online* may expect only *online* hours.
-
-- A training of type *mixed* may expect both *on-site* and *online* hours.
-
-A **training group** is an event that has a training action linked. You can
-have as many training groups as you wish for each training action.
-
-A **tutor** is a special teacher that oversees the whole group. Whenever a
-student has a doubt in a non-on-site class, they usually reach a tutor (via
-email, phone, etc.) to ask them. Also, tutors may actively review the students'
-progress. You can have as many tutors as you want for each training group.
-
-All these things can be configured as you wish.
-""",
     "depends": [
         "event",
     ],
